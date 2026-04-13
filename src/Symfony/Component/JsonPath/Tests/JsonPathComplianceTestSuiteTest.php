bde244c940518e4d93406020d4df8ced1cb1cfa8
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-04-13 17:40:04 +0200

    CS fixes

--- src/Symfony/Component/JsonPath/Tests/JsonPathComplianceTestSuiteTest.php
+++ src/Symfony/Component/JsonPath/Tests/JsonPathComplianceTestSuiteTest.php
@@ -117,9 +117,7 @@ final class JsonPathComplianceTestSuiteTest extends TestCase
     private static function convertToArray(mixed $value): mixed
     {
         if ($value instanceof \stdClass) {
-            return array_map(static function ($val) {
-                return self::convertToArray($val);
-            }, get_object_vars($value));
+            return array_map(static fn ($val) => self::convertToArray($val), get_object_vars($value));
         }
 
         if (\is_array($value)) {
